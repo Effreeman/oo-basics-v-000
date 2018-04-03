32c08999734ca2d5a7abb6a63ae1ef09217aa70a
Emma Freeman <freemoe04@gmail.com>
2018-04-03 19:29:30 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,42 +1,41 @@
 
 class Book
-  
-  def initialize(title)
-    @title = title 
-  end 
 
-  def title 
+  def initialize(title)
+    @title = title
+  end
+
+  def title
     @title
-  end 
+  end
 
   def author=(author)
     @author = author
-  end 
+  end
 
-  def author 
-    @author 
-  end 
+  def author
+    @author
+  end
 
   def page_count=(num)
     @page_count = num
-  end 
+  end
 
-  def page_count 
+  def page_count
     @page_count
   end
 
   def genre=(genre)
     @genre = genre
-  end 
+  end
 
-  def genre 
+  def genre
     @genre
-  end 
+  end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
-  end 
-    
+  end
 
-end 
 
+end
